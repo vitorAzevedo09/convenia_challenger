73b697d38d51f7a5050ae52abcd7113277fec77f
Vitor Silva <vitor.azevedo009@gmail.com>
2021-10-20 19:14:36 -0300

    Initial project setup with sass pre-processor, vue-router and vuex. Background color and card base done too

---
+++ le_tip/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+
+<style scoped lang="sass">
+$font-stack: 'Avenir', Helvetica, Arial, sans-serif
+$primary-color: #9D11F4
+
+#app
+  position: absolute
+  left: 0px
+  top: -9vh
+  font-family: $font-stack
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  background: $primary-color
+  margin-top: 60px
+  width: 100vw
+  height: 100vh
+</style>
---
+++ le_tip/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div class="card">le/tip</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+<style lang="sass" scoped>
+.card
+  background: white
+  position: relative
+  top: 10vh
+  left: 20vw
+  width: 60vw
+  height: 80vh
+  box-shadow: 0 0 300px rgba(0, 0, 0, .3)
+  border-radius: 15px
+</style>
