815890e5438c417046a938a6b148753a3712cdc4
Vitor Silva <vitor.azevedo009@gmail.com>
2023-11-29 14:14:45 -0300

    Remaking in 2023 'letip'

---
+++ letip/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="app">
+    <div class="app_wrapper">
+      <div class="app_title">
+        <h1>Le Tip</h1>
+        <h3>Making your table tip easier</h3>
+      </div>
+      <div class="app_content">
+        TESTE
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../node_modules/typeface-roboto/index.css';
+
+$main-brand-color: #0000ff;
+
+body {
+  font-family: "Roboto";
+  color: $main-brand-color;
+}
+</style>
