d528fa9e9557607c05ac0727ccfc290f77b7f5df
Vitor Silva <vitor.azevedo009@gmail.com>
2023-11-29 15:21:12 -0300

    (chore) Icons: Added font-awesome icons library on package.json

--- letip/src/App.vue
+++ letip/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app" class="app">
-    <div class="app_wrapper">
-      <div class="app_title">
-        <h1>Le Tip</h1>
-        <h3>Making your table tip easier</h3>
+    <div class="app__wrapper">
+      <div class="app__title">
+        <h1>Le Tip
+          <link rel="icon" type="image/x-icon" href="../public/favicon.ico">
+        </h1>
+        <h6>Making tipping easier</h6>
       </div>
-      <div class="app_content">
+      <div class="app__content">
         TESTE
       </div>
     </div>
@@ -23,10 +25,36 @@ export default {
 <style lang="scss">
 @import '../node_modules/typeface-roboto/index.css';
 
-$main-brand-color: #0000ff;
+// Split-Complementary Palette Gold Coin Color: HEX #E1B530 / RGB (225, 181, 48)
 
-body {
+$coin-color: #E1B530;
+$eucalyptus-color: #30E1B5;
+$purple-x11: #B530E1;
+
+$main-brand-color: $coin-color;
+
+body,
+* {
   font-family: "Roboto";
   color: $main-brand-color;
+  margin: 0px;
+  padding: 0px;
+}
+
+.app {
+  display: flex;
+  justify-content: center;
+
+  &__wrapper {
+    flex-flow: column;
+    align-items: center;
+    justify-content: center;
+    margin: auto;
+  }
+
+  &__title {
+    text-align: center;
+  }
+
 }
 </style>
