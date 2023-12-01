7cd4440685f8192302d51a733af4ebc07ee8d4c4
Vitor Silva <vitor.azevedo009@gmail.com>
2023-12-01 17:04:02 -0300

    (chore): loading api_key_swop on code

--- letip/src/vue-apollo.js
+++ letip/src/vue-apollo.js
@@ -5,7 +5,7 @@ const cache = new InMemoryCache()
 
 const apolloClient = new ApolloClient({
   cache,
-  uri: 'https://swop.cx/graphql?api-key=fe98efacaa1362c9db6ede986e21c9061aca16cf3d6b85e08d94f27b22e18b3c',
+  uri: `https://swop.cx/graphql?api-key=${process.env.SWOP__API_KEY}`,
 })
 
 const apolloProvider = createApolloProvider({
